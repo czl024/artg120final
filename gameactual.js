496e81250443e616babbf29f6bd49f9a0354bff5
czl024 <czedl024@gmail.com>
2023-05-24 22:57:24 -0700

    added placeholders and cleaned things up

--- gameactual.js
+++ gameactual.js
@@ -2,13 +2,25 @@ class Test extends GameScene{
     
 }
 
+
+
+class GameMenu extends MenuScene{
+    
+}
+
+
+
 class MainMenu extends Phaser.Scene{
     
 }
 
+
+
 class Loader extends Phaser.Scene{
     
 }
+
+
 
 const game = new Phaser.Game({
     scale: {
@@ -17,6 +29,7 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Loader, MainMenu, Hallway, Hydroponics, CargoBay, LifeSupport, EngineRoom, Bridge, OutroNormal, OutroSpecial],
-    title: "Space Maintenance",
+    //every scene needs to be in here, so dont forget
+    scene: [Loader, MainMenu, ],
+    title: "to be determined",
 });
